08016b71cd1bb97090e8c6a28d05c0fde455aa22
Stefanos Kornilios Mitsis Poiitidis <skmp@emudev.org>
2025-10-15 11:45:52 +0300

    update genrator, add bins

--- tests/ORR_1.s
+++ tests/ORR_1.s
@@ -3,6 +3,23 @@
 .equ BAD_Rd,	0x10
 .equ BAD_Rn,	0x20
 .equ VARBASE,	0x80000
+
+.align 3
+var64:		.word 0x11223344,0x55667788
+
+rdVal:		.word 0
+rnVal:		.word 0
+memVal:		.word 0
+
+.align 2
+exceptionFlag: .word 0
+
+romvar:  	.byte 0x80,0,0,0
+romvar2: 	.byte 0x00,0x8f,0,0xff
+romvar3: 	.byte 0x80,0x7f,0,0
+
+.global start
+start:
 	mov 	r1,#0
 	mov 	r2,#2
 	mov 	r3,#3
